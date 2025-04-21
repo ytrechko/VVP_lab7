6eccd1edb40e974bc8bcec1a63641c85329118b0
”Slava <yvi86248624@gmail.>
2025-04-21 08:55:38 +0300

    Добавление класса Weather и описанеие его полей с конструктором по умолчанию

---
+++ WeatherService/Weather.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <string>
+
+using namespace std;
+class Weather
+{
+	string city;
+	double lon;
+	double lat;
+	double temperature;
+	string weather;
+	double windSpeed;
+	int clouds;
+public:
+	Weather() :city("Киров"),
+		lon(49.6601), lat(58.5966),
+		temperature(5.69),
+		weather("дождь"),
+		windSpeed(4.27),
+		clouds(100) {}
+};
+
