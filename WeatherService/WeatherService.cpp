aa5918b959cbff29b9b239cb4d5039e270b6deb7
”Slava <yvi86248624@gmail.>
2025-04-21 10:19:18 +0300

    разрешил конфликты

--- WeatherService/WeatherService.cpp
+++ WeatherService/WeatherService.cpp
@@ -1,31 +1,7 @@
 ﻿#include <iostream>
-<<<<<<< HEAD
-#include "JsonService.h"
 
 int main()
 {
-	try {
-		JsonService js;
-		Weather w = js.getWeather("weather.json");
-		int i;
-	}
-	catch (exception& e) {
-		cout << e.what();
-	}
-=======
-#include "XmlService.h"
-
-int main()
-{
-    try {
-        XmlService xs;
-        Weather w = xs.getWeather("weather.xml");
-        int u;
-    }
-    catch (exception& e) {
-        cout << e.what();
-    }
-
->>>>>>> feature-xml
+    std::cout << "Hello World!\n";
 }
 
