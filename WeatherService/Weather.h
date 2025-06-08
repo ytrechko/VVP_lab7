5f56ff8afff25fe7042172ec7eb7bddb802c859a
ytrechko <yvi86248624@gmail.com>
2025-06-08 04:31:11 +0300

    Update Weather.h

--- WeatherService/Weather.h
+++ WeatherService/Weather.h
@@ -12,8 +12,8 @@ class Weather
 	double windSpeed;
 	int clouds;
 public:
-	Weather(string city,
-<<<<<<< HEAD
+	Weather(
+	string city,
 	double lon,
 	double lat,
 	double temperature,
@@ -21,36 +21,12 @@ public:
 	double windSpeed,
 	int clouds) :
 		city(city),
-=======
-		double lon,
-		double lat,
-		double temperature,
-		string weather,
-		double windSpeed,
-		int clouds) : city(city),
->>>>>>> feature-xml
 		lon(lon),
 		lat(lat),
 		temperature(temperature),
 		weather(weather),
 		windSpeed(windSpeed),
-<<<<<<< HEAD
 		clouds(clouds){}
-	Weather() {}
-	//:city("Киров"),
-	//	lon(49.6601), lat(58.5966),
-	//	temperature(5.69),
-	//	weather("дождь"),
-	//	windSpeed(4.27),
-	//	clouds(100) {}
-=======
-		clouds(clouds) {};
-	Weather() :city("Киров"),
-		lon(49.6601), lat(58.5966),
-		temperature(5.69),
-		weather("дождь"),
-		windSpeed(4.27),
-		clouds(100) {}
->>>>>>> feature-xml
+
 };
 
