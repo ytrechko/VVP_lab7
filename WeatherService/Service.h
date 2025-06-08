3d336071b9ed85e8f15e2ed7939f027c5e0f8733
ytrechko <yvi86248624@gmail.com>
2025-06-08 04:23:00 +0300

    Update Service.h

--- WeatherService/Service.h
+++ WeatherService/Service.h
@@ -1,14 +1,8 @@
 #pragma once
 #include "Weather.h"
-<<<<<<< HEAD
-
 #include <string>
 
 class Service : public Weather
-=======
-#include <string>
-class Service
->>>>>>> feature-xml
 {
 public:
 	virtual Weather getWeather(std::string s) = 0;
